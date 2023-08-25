6368f587d86564ea1c5f6e26078c324498c6659f
Koen De Groot <koen.de.groot@organimmo.be>
2023-08-25 16:23:38 +0200

    added nav btns

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -7,7 +7,8 @@
       </div>
     </div>
     <div class="navbar-btns">
-      <button class="add-new-task-btn"></button>
+      <button class="add-new-task-btn">+ Add New Task</button>
+      <button><img src="../assets/images/icon-vertical-ellipsis.svg" alt="ellipsis" /></button>
     </div>
   </div>
 </template>
@@ -20,20 +21,33 @@
   display: flex;
   justify-content: space-between;
   padding: 1.25rem 2rem;
-}
-.logo {
-  display: flex;
-  align-items: center;
-  gap: 3.875rem;
-  .logo-img {
-    background-image: var(--logo);
-    background-repeat: no-repeat;
-    width: 160.528px;
-    height: 25.224px;
+
+  .logo {
+    display: flex;
+    align-items: center;
+    gap: 3.875rem;
+    .logo-img {
+      background-image: var(--logo);
+      background-repeat: no-repeat;
+      width: 160.528px;
+      height: 25.224px;
+    }
+    .logo-title {
+      h1 {
+        font-size: 1.5rem;
+        font-weight: 700;
+      }
+    }
   }
-  .logo-title {
-    h1 {
-      font-size: 1.5rem;
+
+  .navbar-btns {
+    gap: 1.5rem;
+    display: flex;
+
+    .add-new-task-btn {
+      background-color: var(--mainPurple);
+      padding: 1rem 1.5rem;
+      font-size: 0.9375rem;
       font-weight: 700;
     }
   }
