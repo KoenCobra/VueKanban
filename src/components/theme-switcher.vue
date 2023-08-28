e96de8853915dd5595c36b037d8c353cbf835f15
Koen De Groot <koen.de.groot@organimmo.be>
2023-08-28 09:41:51 +0200

    added primevue input switch

--- src/components/theme-switcher.vue
+++ src/components/theme-switcher.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="theme-switch">
     <img src="../assets/images/icon-light-theme.svg" alt="light-theme" />
-    <input type="checkbox" />
+    <InputSwitch v-model="checked" />
     <img src="../assets/images/icon-dark-theme.svg" alt="dark-theme" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InputSwitch from 'primevue/inputswitch'
+import { ref } from 'vue'
+
+const checked = ref(false)
+</script>
 
 <style scoped lang="scss">
 @import '../assets/sass/variables.scss';
