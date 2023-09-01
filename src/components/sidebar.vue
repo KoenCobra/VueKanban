13f5bbeb93bb3bd78e7686cb4ce8a55a1b4fdf49
Koen De Groot <koen.de.groot@organimmo.be>
2023-09-01 14:14:08 +0200

    added reset of new board values

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -23,7 +23,7 @@
             {{ board.name }}
           </li>
         </ul>
-        <button class="new-board-btn" @click="isDialogVisible = true">
+        <button class="new-board-btn" @click="openDialog">
           <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z"
@@ -74,7 +74,7 @@
 <script setup lang="ts">
 import ThemeSwitcher from '@/components/theme-switcher.vue'
 import { useThemeStore } from '@/stores/themeStore'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 import { useBoardStore } from '@/stores/boardStore'
 import GenericDialog from '@/components/generic-dialog.vue'
 import * as Yup from 'yup'
@@ -87,11 +87,7 @@ const themeStore = useThemeStore()
 const isSideBarVisible = ref(true)
 const isDialogVisible = ref(false)
 
-const newBoard = ref<Board>({} as Board)
-
-onMounted(() => {
-  newBoard.value = { name: '', columns: [{ name: 'ToDo' }, { name: 'Doing' }] }
-})
+const newBoard = ref<Board>({ name: '', columns: [{ name: 'ToDo' }, { name: 'Doing' }] })
 
 const addColumn = () => {
   newBoard.value.columns.push({ name: '' })
@@ -104,6 +100,11 @@ const schema = Yup.object().shape({
 const onSubmit = async (values: any) => {
   newBoard.value.name = values.name
   boardStore.boards.push(newBoard.value)
+}
+
+const openDialog = () => {
+  newBoard.value = { name: '', columns: [{ name: 'ToDo' }, { name: 'Doing' }] }
+  isDialogVisible.value = true
 }
 </script>
 
